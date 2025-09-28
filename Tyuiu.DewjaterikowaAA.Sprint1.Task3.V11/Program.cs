115ce39dc02b886b831947bdb38819ad284afbbc
Anastasia24057 <nastya.dewa@gmail.com>
2025-09-28 20:14:06 +0500

    Task3

---
+++ Tyuiu.DewjaterikowaAA.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tyuiu.DewjaterikowaAA.Sprint1.Task3.V11.Lib;
+
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #1 | Выполнила: Девятерикова А.А. | ИИПБ - 25 - 1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #11                                                             *");
+Console.WriteLine("* Выполнила: Девятерикова Анастасия Андреевна | ИИПБ - 25 - 1             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("* Напишите программу, которая вычисляет площадь треугольника, если        *");
+Console.WriteLine("* известны координаты его углов.                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x1 = -2;
+double y1 = 5;
+double x2 = 1;
+double y2 = 7;
+double x3 = 5;
+double y3 = -3;
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Площадь треугольника = " + ds.TriangleArea(x1, y1, x2, y2, x3, y3));
+Console.ReadKey();
+
+
