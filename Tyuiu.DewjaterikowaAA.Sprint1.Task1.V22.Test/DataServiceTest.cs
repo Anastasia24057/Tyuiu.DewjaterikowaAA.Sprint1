a4198b8dec03b2372f3d21dc43695f1284bb8c7e
Anastasia24057 <nastya.dewa@gmail.com>
2025-09-28 17:56:10 +0500

    Task1

---
+++ Tyuiu.DewjaterikowaAA.Sprint1.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint1.Task1.V22.Lib;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint1.Task1.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3, res);
+
+        }
+    }
+}
